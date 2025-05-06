d98a865b038d854a1e2bb0f84a8430f2af81a377
GabrielLohnScheffer <160729924+GabrielLohnScheffer@users.noreply.github.com>
2025-05-06 12:26:53 +0000

    site finalizado

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Tropicalia-GabrielLohnScheffer
+# Site acessível sobre Tropicália
+## Sobre
+Refatoração de um site implementando recursos de acessibilidade no HTML, CSS e JS.
+## Recursos de acessibilidade
+- Atributos aria
+- Alt
+- Tab-index
+- Menu de acessibilidade
+## Tecnologias utilizadas
+- Bootstrap
+- ScrollReveal.js
+- HTML
+- CSS
+- JS
--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
     const alternaContraste = document.getElementById('alterna-contraste');
+    const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
 
     let tamanhoAtualFonte = 1;
     
@@ -26,5 +28,10 @@ document.addEventListener('DOMContentLoaded', function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
     });
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.js"></script>
 });
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
